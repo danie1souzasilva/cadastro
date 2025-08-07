4344df00609c69a49b5692220a4de993b47d9d5f
F2255242 Daniel De Souza Silva <danielsouzasilva@bb.com.br>
2025-08-07 08:20:56 -0300

    commits que era pra fazer ontem

--- IdeaProjects/JavaLangObject/src/main/java/org/example/Main.java
+++ IdeaProjects/JavaLangObject/src/main/java/org/example/Main.java
@@ -5,5 +5,15 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("olá mundo");
+
+        int [] idades = new int[5];
+        for(int i = 0; i < idades.length; i++){
+            idades[i] = i;
+            System.out.println(i);
+
+        }
+
+        System.out.println("essas aulas ensinaram sobre pacote, comentarios, visibilidade...");
     }
+
 }
